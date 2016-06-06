0cc76ccc8f4d44a9e71daab5bdc2e9a0bdb49917
Mikko Tossavainen <mikko.tossavainen@hotmail.com>
2016-06-06 12:32:51 +0300

    Some work on spawner and enemy

---
+++ PirateGaem/Assets/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject Enemy;
+
+	// Use this for initialization
+	void Start () {
+		SpawnEnemy ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnEnemy() {
+		Instantiate (Enemy);
+	}
+}
