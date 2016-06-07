6779c27f8ad4516f9613a325ad7b457581b8ffa6
Vierailutunnus <Guest@KC402LAB03.local>
2016-06-07 10:56:19 +0300

    - Added Scenes to Build Settings - Added Working buttons to mainmenu

---
+++ PirateGaem/Assets/Scripts/Menus/MainMenuActions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuActions : MonoBehaviour {
+
+	public void onPlay(){
+		SceneManager.LoadScene(1);
+	}
+	public void onExit(){
+		Application.Quit();
+	}
+}
